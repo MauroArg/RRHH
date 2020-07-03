29c24d3aedf3ac5d03e5bc6d2fb6787739c27add
Gustavo <>
2020-07-03 13:15:51 -0600

    Estructuración de paquetes(app,entity,utility) y creación de clases iniciales. Inclusión de clase Encryption.

---
+++ src/com/bitlab/app/Client.java
@@ -0,0 +1,19 @@
+package com.bitlab.app;
+import com.bitlab.utility.Encryption;
+import java.util.Scanner;
+
+/**
+ * @author Gustavo Gómez
+ * class: Socket
+ * fecha: 2020-07-02
+ */
+public class Client {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            
+            
+    }
+    
+}
---
+++ src/com/bitlab/entity/Employe.java
@@ -0,0 +1,13 @@
+package com.bitlab.entity;
+
+/**
+ *
+ * @author Maury
+ */
+public class Employe 
+{
+
+    public Employe() {
+        //Comentario de Mauro  
+    }
+}
